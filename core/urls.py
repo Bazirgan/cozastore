b98285a45b7de9d6001dce4469799576b24f4d9a
Bazirgan <bazirgan@gmail.com>
2024-02-07 18:50:12 +0300

    navbar update

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from core.views import index, product, shopingcart, blog, about, contact
 
 urlpatterns = [
-    path('index.html', index, name='index'),
-    path('product', product, name='product'),
-    path('shoping-cart.html', shopingcart, name='shopingcart'),
-    path('blog.html', blog, name='blog'),
-    path('about.html', about, name='about'),
-    path('contact.html', contact, name='contact')
+    path('index/',index, name='index'),
+    path('product/',product, name='product'),
+    path('shopingcart/',shopingcart, name='shopingcart'),
+    path('blog/',blog, name='blog'),
+    path('about/',about, name='about'),
+    path('contact/',contact, name='contact')
 ]
 
 
