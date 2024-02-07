65afb57a72426b58e645b31503d4b8a51b65a5eb
Bazirgan <bazirgan@gmail.com>
2024-02-07 16:48:15 +0300

    navbar ,admin panel update

--- core/admin.py
+++ core/admin.py
@@ -4,5 +4,20 @@ from core.models import Category, Product, Color
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Product)
 admin.site.register(Color)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'size')
+    search_fields = ('name', 'price', 'category__name', 'color__name')
+    readonly_fields = ('like',)
+    fieldsets = (
+        ('Main Information', {
+            'fields': ('name', 'price', 'category', 'color', 'size')
+        }),
+        ('Main Information', {
+            'fields': ('like', )
+        }),
+    )
+    ordering = ('-created_at',)
+    
+admin.site.register(Product,ProductAdmin)
